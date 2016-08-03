ad2590d5be3813c6d3db5e1a4972044dc829d5e7
Charles Cosse <ccosse@gmail.com>
2016-08-03 13:56:59 -0700

    country_codes

--- models.py
+++ models.py
@@ -1,3 +1,18 @@
+from __future__ import unicode_literals
 from django.db import models
+import os
+from tinymce.models import HTMLField
 
-# Create your models here.
+class NetworkEvent(models.Model):
+    """
+    src_ip
+    src_idn
+    dst_ip
+    dst_idn
+    src_port
+    dst_port
+    tstamp
+    country_idn
+    """
+    lat	= models.FloatField(blank=True)
+	lon	= models.FloatField(blank=True)
