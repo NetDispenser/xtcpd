8b8cf763ebcf34922966ddea4601eb6d7cebb4ed
Charles Cosse <ccosse@gmail.com>
2016-08-04 08:49:19 -0700

    NetworkEvent model

--- models.py
+++ models.py
@@ -2,16 +2,15 @@ from __future__ import unicode_literals
 from django.db import models
 import os
 from tinymce.models import HTMLField
-"""
+
 class NetworkEvent(models.Model):
-    src_ip
-    src_idn
-    dst_ip
-    dst_idn
-    src_port
-    dst_port
-    tstamp
-    country_idn
+    src_ip=models.GenricIPAddressField()
+    src_idn=models.PositiveIntegerField()
+    dst_ip=models.GenricIPAddressField()
+    dst_idn=models.PositiveIntegerField()
+    src_port=models.PositiveIntegerField()
+    dst_port=models.PositiveIntegerField()
+    tstamp=TimeField(auto_now=True)
+    country_idn=models.PositiveIntegerField()
     lat	= models.FloatField(blank=True)
 	lon	= models.FloatField(blank=True)
-"""
