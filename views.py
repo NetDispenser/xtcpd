b5877080c79f4c3dbbc69fcf2cb3b3e4865f83ba
root <root@hotspot.asymptopia.org>
2016-08-16 16:11:36 -0600

    prep for electron frontend

--- views.py
+++ views.py
@@ -13,13 +13,22 @@ from django.contrib.auth.decorators import login_required
 FORMAT = '%(asctime)-15s %(message)s'
 logging.basicConfig(filename='/var/log/xtcpd/xtcpd.log',level=logging.DEBUG, format=FORMAT)
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
 def xtcpd(request):
+	logging.debug(request.META)
 	try:
 		qs=request.META['QUERY_STRING']
 		if len(qs)>1:
-			s=xmlrpc.client.Server("http://localhost:8000")
+			s=xmlrpc.client.Server("http://hotspot.asymptopia.org:8000")
 			if qs=='get_data':
-				rval=s.get_data()
+				rval=s.get_data((get_client_ip(request),))
 			elif qs=='toggle_debug':
 				rval=s.toggle_debug()
 			elif qs=='toggle_running':
